cd4321660bcd297df916fb7128bcc8589b8647e2
steve <stevemighty2030@gmail.com>
2022-08-27 10:18:59 +0300

    the assignment is complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const init = () => {
-  
+    grabFormElement();
+}
+
+function grabFormElement() {
+    let form = document.querySelector('form');
+    form.addEventListener('submit', displayMovie);
+}
+
+function displayMovie(e) {
+    e.preventDefault();
+    let titleHead = document.querySelector('#movieDetails h4');
+    let summaryParagraph = document.querySelector('#movieDetails p');
+    let inputValue = document.querySelector('#searchByID').value;
+    fetch(`http://localhost:3000/movies/${inputValue}`)
+        .then(res => res.json())
+        .then(movie => {
+            titleHead.innerText = movie.title;
+            summaryParagraph.innerText = movie.summary;
+
+        })
+
+
+    e.target.reset();
 }
 
 document.addEventListener('DOMContentLoaded', init);
